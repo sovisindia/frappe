e1274c3a942a20671b046d8604fce396a94b10ef
Bhuvnesh <bhuvankumar9944@gmail.com>
2024-07-14 12:05:53 +0530

    add: PDF / Excel from reports

--- frappe/public/js/frappe/microtemplate.js
+++ frappe/public/js/frappe/microtemplate.js
@@ -181,40 +181,46 @@ frappe.render_template = function (name, data) {
 		w.document.close();
 	});
 
-frappe.render_pdf = function (html, opts = {}) {
-	//Create a form to place the HTML content
-	var formData = new FormData();
+frappe.render_pdf = function (html, opts = {}, download = true) {
+	return new Promise((res, rej) => {
+		//Create a form to place the HTML content
+		var formData = new FormData();
 
-	//Push the HTML content into an element
-	formData.append("html", html);
-	if (opts.orientation) {
-		formData.append("orientation", opts.orientation);
-	}
-	var blob = new Blob([], { type: "text/xml" });
-	formData.append("blob", blob);
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/api/method/frappe.utils.print_format.report_to_pdf");
-	xhr.setRequestHeader("X-Frappe-CSRF-Token", frappe.csrf_token);
-	xhr.responseType = "arraybuffer";
-
-	xhr.onload = function (success) {
-		if (this.status === 200) {
-			var blob = new Blob([success.currentTarget.response], { type: "application/pdf" });
-			var objectUrl = URL.createObjectURL(blob);
-
-			// Create a hidden a tag to force set report name
-			// https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
-			let hidden_a_tag = document.createElement("a");
-			document.body.appendChild(hidden_a_tag);
-			hidden_a_tag.style = "display: none";
-			hidden_a_tag.href = objectUrl;
-			hidden_a_tag.download = opts.report_name || "report.pdf";
-
-			// Open report in a new window
-			hidden_a_tag.click();
-			window.URL.revokeObjectURL(objectUrl);
+		//Push the HTML content into an element
+		formData.append("html", html);
+		if (opts.orientation) {
+			formData.append("orientation", opts.orientation);
 		}
-	};
-	xhr.send(formData);
+		var blob = new Blob([], { type: "text/xml" });
+		formData.append("blob", blob);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "/api/method/frappe.utils.print_format.report_to_pdf");
+		xhr.setRequestHeader("X-Frappe-CSRF-Token", frappe.csrf_token);
+		xhr.responseType = "arraybuffer";
+
+		xhr.onload = function (success) {
+			if (this.status === 200) {
+				if (download) {
+					var blob = new Blob([success.currentTarget.response], { type: "application/pdf" });
+					var objectUrl = URL.createObjectURL(blob);
+
+					// Create a hidden a tag to force set report name
+					// https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
+					let hidden_a_tag = document.createElement("a");
+					document.body.appendChild(hidden_a_tag);
+					hidden_a_tag.style = "display: none";
+					hidden_a_tag.href = objectUrl;
+					hidden_a_tag.download = opts.report_name || "report.pdf";
+
+					// Open report in a new window
+					hidden_a_tag.click();
+					window.URL.revokeObjectURL(objectUrl);
+				}
+
+				res(success.currentTarget.response);
+			} else rej()
+		};
+		xhr.send(formData);
+	});
 };
